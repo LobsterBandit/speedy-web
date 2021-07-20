c4689c9d0639e6ce465e871ea74a3537dd888f9c
lobsterbandit <code.lobsterbandit@outlook.com>
2021-07-19 20:02:30 -0400

    Initial app scaffold

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import speedy from "./speedy.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={speedy} className="App-logo" alt="speedy" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
