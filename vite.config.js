4d3bdfccee751f20b4111853ba2a534a9b3f2dd2
LobsterBandit <code.lobsterbandit@outlook.com>
2021-08-02 17:58:09 -0400

    Report bundle stats (#9)
    * Rollup bundle stats plugin
    
    * Move build-only plugins
    
    * Add nvmrc so cf pages uses correct node version

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,17 @@ import reactRefresh from "@vitejs/plugin-react-refresh"
 import { visualizer } from "rollup-plugin-visualizer"
 import { defineConfig } from "vite"
 import reactJsx from "vite-react-jsx"
+import { rollupPluginBundleStats } from "./src/rollup-plugin-bundle-stats"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    reactRefresh(),
-    reactJsx(),
-    visualizer({ filename: "stats/index.html" }),
-  ],
+  plugins: [reactRefresh(), reactJsx()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        rollupPluginBundleStats({ limit: 10 }),
+        visualizer({ filename: "stats/index.html" }),
+      ],
+    },
+  },
 })
